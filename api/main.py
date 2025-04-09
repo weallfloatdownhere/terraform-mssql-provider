f6e3ffaccd4a1e881db8766f9bae69ce998eb27a
David Duchesne <dav.duchesne@cgi.com>
2025-04-09 16:26:54 -0400

    sql

--- api/main.py
+++ api/main.py
@@ -40,6 +40,18 @@ def add_user(group_name: str):
     except Exception as e:
         print(f"ERROR {e}")
 
+#USE Testing
+#GO
+#select name as username,
+#       create_date,
+#       modify_date,
+#       type_desc as type,
+#       authentication_type_desc as authentication_type
+#from sys.database_principals
+#where type not in ('A', 'G', 'R', 'X')
+#      and sid is not null
+#      and name != 'guest'
+#order by username;
 
 def select_user(group_name: str):
     db = SessionLocal()
