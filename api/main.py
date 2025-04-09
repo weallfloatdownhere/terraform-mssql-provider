713ccf9a4b5d8c44dfb9b635e492e5f00a401edd
David Duchesne <dav.duchesne@cgi.com>
2025-04-09 14:37:50 -0400

    yee

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from sqlalchemy.sql import text, quoted_name
 
 # MSSQL Database Connection Details
 DATABASE_URL = "mssql+pyodbc://@localhost\\SQLEXPRESS/testing?driver=ODBC+Driver+17+for+SQL+Server"
@@ -15,7 +16,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # FastAPI App
-app = FastAPI()
+# app = FastAPI()
 
 # Dependency to get DB session
 def get_db():
@@ -25,29 +26,52 @@ def get_db():
     finally:
         db.close()
 
-# User Model
-class User(Base):
-    __tablename__ = "users"
+def add_user(group_name: str):
+    db = SessionLocal()
+    try:
+        db.execute(text(f"CREATE LOGIN {quoted_name(group_name, False)} WITH PASSWORD = '{group_name}'"))                       
+        db.commit()
+    except Exception as e:
+        print(f"ERROR {e}")
     
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
+    try:
+        db.execute(text(f"CREATE USER {quoted_name(group_name, False)} FOR LOGIN {group_name}"))                        
+        db.commit()
+    except Exception as e:
+        print(f"ERROR {e}")
 
-# Create database tables
-Base.metadata.create_all(bind=engine)
 
-# Routes
-@app.post("/users/")
-def create_user(name: str, email: str, db: Session = Depends(get_db)):
-    new_user = User(name=name, email=email)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+USERNAME = "testing1"
+add_user(USERNAME)
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == user_id).first()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+## User Model
+#class User(Base):
+#    __tablename__ = "users"
+#    
+#    id = Column(Integer, primary_key=True, index=True)
+#    name = Column(String)
+#    email = Column(String)
+#
+#
+## Create database tables
+#Base.metadata.create_all(bind=engine)
+#
+## Routes
+#@app.post("/add_group")
+#def create_user(group: str, db: Session = Depends(get_db)):
+#    add_user(group, db)
+#
+#@app.post("/users/")
+#def create_user(name: str, email: str, db: Session = Depends(get_db)):
+#    new_user = User(name=name, email=email)
+#    db.add(new_user)
+#    db.commit()
+#    db.refresh(new_user)
+#    return new_user
+#
+#@app.get("/users/{user_id}")
+#def read_user(user_id: int, db: Session = Depends(get_db)):
+#    user = db.query(User).filter(User.id == user_id).first()
+#    if user is None:
+#        raise HTTPException(status_code=404, detail="User not found")
+#    return user
