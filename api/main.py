217cec1a45c41fb541de5812f76196a24c15e006
David Duchesne <dav.duchesne@cgi.com>
2025-04-14 11:29:59 -0400

    main function

--- api/main.py
+++ api/main.py
@@ -50,29 +50,6 @@ def connect_to_database(server: str, database: str):
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)                                    # Initialize the Session local
     return SessionLocal                                                                                            # Return the session
 
-def get_group_from_database(group_name: str, server: str, database: str, SessionLocal: Session):
-    db      = SessionLocal()                                                                                       # Initialize session
-    roles   = alias(database_principals, name="roles")                                                             # Initialize the aliase `roles`
-    members = alias(database_principals, name="members")                                                           # Initialize the aliases `members`
-    query = (                                                                                                      # Build the query
-        select(
-            roles.c.principal_id.label("RolePrincipalID"),
-            roles.c.name.label("RolePrincipalName"),
-            database_role_members.c.member_principal_id.label("MemberPrincipalID"),
-            members.c.name.label("MemberPrincipalName"),
-            literal(server).label("Server"),
-            literal(database).label("Database")
-        )
-        .join(database_role_members, database_role_members.c.role_principal_id == roles.c.principal_id)
-        .join(members, database_role_members.c.member_principal_id == members.c.principal_id)
-        .where(members.c.name == group_name)
-    )
-    result = db.execute(query)                                                                                     # Execute the query
-    #returned = []
-    #or row in result:
-    #   returned += [{"group_name": row.MemberPrincipalName, "role_name": row.RolePrincipalName }]
-    return result.all()
-
 # Group exists function in sys.sql_logins
 def is_group_exists_in_sql_logins(group_name: str, SessionLocal: Session):
     db = SessionLocal()                                                                                            # Initialize session
@@ -116,52 +93,63 @@ def is_role_attributed(group_name: str, role_name: str, SessionLocal: Session):
     else:                                                                                                          # Return 0 the result the role is not already attributed
         return 0
 
+def get_group_from_database(group_name: str, server: str, database: str, SessionLocal: Session):
+    db      = SessionLocal()                                                                                       # Initialize session
+    roles   = alias(database_principals, name="roles")                                                             # Initialize the aliase `roles`
+    members = alias(database_principals, name="members")                                                           # Initialize the aliases `members`
+    query = (                                                                                                      # Build the query
+        select(
+            roles.c.principal_id.label("RolePrincipalID"),
+            roles.c.name.label("RolePrincipalName"),
+            database_role_members.c.member_principal_id.label("MemberPrincipalID"),
+            members.c.name.label("MemberPrincipalName"),
+            literal(server).label("Server"),
+            literal(database).label("Database")
+        )
+        .join(database_role_members, database_role_members.c.role_principal_id == roles.c.principal_id)
+        .join(members, database_role_members.c.member_principal_id == members.c.principal_id)
+        .where(members.c.name == group_name)
+    )
+    result = db.execute(query)                                                                                     # Execute the query
+    return result.all()
+
 # Adding group the the sql_logins
-def add_group_to_sql_logins(group_name: str, SessionLocal: Session):
-    if is_group_exists_in_sql_logins(group_name, SessionLocal) <= 0:                                               # Check if the group exists in sql_logins before adding it
-        db = SessionLocal()                                                                                        # Initialize session
-        try:                                                                                                       # Try to add the user to the sql_logins
-            db.execute(text(f"CREATE LOGIN {quoted_name(group_name, False)} WITH PASSWORD = '{group_name}'"))      # Execute the query                       
-            db.commit()                                                                                            # Commit to database
-            return 0                                                                                               # return 0 if the query as been succesfull
-        except Exception as e:
-            db.close()                                                                                             # Close the database connection i case of error
-            print(f"ERROR {e}")                                                                                    # Print the error
-            return 0                                                                                               # return 0 if the query as not been succesfull
-    else:
-        return 1
+def add_group_to_sql_logins(group_name: str, SessionLocal: Session):                                               # Check if the group exists in sql_logins before adding it
+    db = SessionLocal()                                                                                            # Initialize session
+    try:                                                                                                           # Try to add the user to the sql_logins
+        db.execute(text(f"CREATE LOGIN {quoted_name(group_name, False)} WITH PASSWORD = '{group_name}'"))          # Execute the query                       
+        db.commit()                                                                                                # Commit to database
+        return 0                                                                                                   # return 0 if the query as been succesfull
+    except Exception as e:
+        db.close()                                                                                                 # Close the database connection i case of error
+        print(f"ERROR {e}")                                                                                        # Print the error
+        return 1                                                                                                   # return 1 if the query as not been succesfull
 
 # Adding group the the database_principals
 # TODO: CREATE USER GROUPNAME FROM EXTERNAL PROVIDER
 def add_group_to_database_principals(group_name: str, SessionLocal: Session):           
-    if is_group_exists_in_the_database_principals(group_name, SessionLocal) <= 0:                                  # Check if the group exists in database_principals before adding it
-        db = SessionLocal()                                                                                        # Initialize session
-        try:                                                                                                       # Try to add the user to the sql_logins
-            db.execute(text(f"CREATE USER {quoted_name(group_name, False)} FOR LOGIN {group_name}"))               # Execute the query                         
-            db.commit()                                                                                            # Commit to database  
-            return 0                                                                                               # return 0 if the query as been succesfull
-        except Exception as e:
-            db.close()                                                                                             # Close the database connection i case of error
-            print(f"ERROR {e}")                                                                                    # Print the error
-            return 1                                                                                               # return 1 if the query as not been succesfull
-    else:
-        return 1
-    
-# Attribute role to the group (PUT)
-def add_role_to_group(group_name: str, role_name: str, SessionLocal: Session):           
-    if is_role_attributed(group_name, role_name, SessionLocal) <= 0:                                               # Check if the role is already attributed
-        db = SessionLocal()                                                                                        # Initialize session
-        try:                                                                                                       # Try to attribute the role
-            db.execute(text(f"ALTER ROLE {role_name} ADD MEMBER {group_name}"))                                    # Execute the query                         
-            db.commit()                                                                                            # Commit to database  
-            return 0                                                                                               # return 0 if the query as been succesfull
-        except Exception as e:
-            db.close()                                                                                             # Close the database connection i case of error
-            print(f"ERROR {e}")                                                                                    # Print the error
-            return 1                                                                                               # return 1 if the query as not been succesfull
-    else:
-        return 1
+    db = SessionLocal()                                                                                            # Initialize session
+    try:                                                                                                           # Try to add the user to the sql_logins
+        db.execute(text(f"CREATE USER {quoted_name(group_name, False)} FOR LOGIN {group_name}"))                   # Execute the query                         
+        db.commit()                                                                                                # Commit to database  
+        return 0                                                                                                   # return 0 if the query as been succesfull
+    except Exception as e:
+        db.close()                                                                                                 # Close the database connection i case of error
+        print(f"ERROR {e}")                                                                                        # Print the error
+        return 1                                                                                                   # return 1 if the query as not been succesfull
 
+# Attribute role to the group (PUT)
+def add_role_to_group(group_name: str, role_name: str, SessionLocal: Session):                                     # Check if the role is already attributed
+    db = SessionLocal()                                                                                            # Initialize session
+    try:                                                                                                           # Try to attribute the role
+        db.execute(text(f"ALTER ROLE {role_name} ADD MEMBER {group_name}"))                                        # Execute the query                         
+        db.commit()                                                                                                # Commit to database  
+        return 0                                                                                                   # return 0 if the query as been succesfull
+    except Exception as e:
+        db.close()                                                                                                 # Close the database connection i case of error
+        print(f"ERROR {e}")                                                                                        # Print the error
+        return 1                                                                                                   # return 1 if the query as not been succesfull
+        
 # Unattribute role from the group (DELETE)
 def delete_role_from_group(group_name: str, role_name: str, SessionLocal: Session):           
     if is_role_attributed(group_name, role_name, SessionLocal) > 0:                                                # Check if the role is already attributed
@@ -193,12 +181,10 @@ def delete_group_from_database_principals(group_name: str, SessionLocal: Session
     else:
         return 1
 
-SERVER     = "localhost\\SQLEXPRESS"                                                                               # MSSQL Server
-DATABASE   = "testing"                                                                                             # MSSQL Database
-GROUP_NAME = "ADSQLGroup4"
-IS_TEST    = False                                                                                                 # Specify if its a test
-ADDING     = False                                                                                                 # Specify if we adding group and role
-DELETE     = False                                                                                                 # Specify if we deleting group and role
+def add_group_and_attribute_role(group_name: str, role_name: str, SessionLocal: Session):
+    if is_group_exists_in_sql_logins(group_name, SessionLocal) <= 0:              add_group_to_sql_logins(group_name, SessionLocal)
+    if is_group_exists_in_the_database_principals(group_name, SessionLocal) <= 0: add_group_to_database_principals(group_name, SessionLocal)
+    if is_role_attributed(group_name, role_name, SessionLocal) <= 0:              add_role_to_group(group_name, role_name, SessionLocal)
 
 ## Routes
 @app.get("/server/{server}/database/{database}/group/{group_name}", response_model=List[GroupShema])
@@ -208,18 +194,13 @@ def get_group(server: str, database:str, group_name: str):
 
 @app.put("/server/{server}/database/{database}/group/{group_name}")
 def update_group(server: str, database:str, group_name: str):
-    # session = connect_to_database(server, database)                                                               # Initialize database session
+    # session = connect_to_database(server, database)                                                             # Initialize database session
     return("UPDATING")
-
-
-# db_accessadmin
 
 @app.post("/server/{server}/database/{database}/group/{group_name}")
 def create_group(server: str, database:str, group_name: str, role_name: str):
     session = connect_to_database(server, database)                                                               # Initialize database session
-    add_group_to_sql_logins(group_name, session)                                                                  # Add the group to sql_logins
-    add_group_to_database_principals(group_name, session)                                                         # Add the group to database_principals
-    add_role_to_group(group_name, role_name, session)                                                             # Attribute the role
+    add_group_and_attribute_role(group_name, role_name, session)                                                  # Add the group to database and attribute role
 
 @app.delete("/server/{server}/database/{database}/group/{group_name}")
 def delete_group(server: str, database:str, group_name: str):
@@ -227,16 +208,22 @@ def delete_group(server: str, database:str, group_name: str):
     delete_group_from_database_principals(group_name, session)                                                    # Delete the group from the MSSQL
 
 
-
-def test():                                                                                                        # Entrypoint function
-    session = connect_to_database(SERVER, DATABASE)                                                                # Initialize database session
-    if ADDING:
-        add_group_to_sql_logins(GROUP_NAME, session)                                                            # Add the group to sql_logins
-        add_group_to_database_principals(GROUP_NAME, session)                                                   # Add the group to database_principals
-        add_role_to_group(GROUP_NAME, "db_accessadmin", session)                                                # Attribute the role
-    if DELETE:
-        delete_role_from_group(GROUP_NAME, "db_accessadmin", session)                                           # Remove the role from the group
-        delete_group_from_database_principals(GROUP_NAME, session)                                              # Delete the group from the MSSQL
-
-if IS_TEST:
-    test()
+# SERVER     = "localhost\\SQLEXPRESS"                                                                            # MSSQL Server
+# DATABASE   = "testing"                                                                                          # MSSQL Database
+# GROUP_NAME = "ADSQLGroup4"
+# IS_TEST    = False                                                                                              # Specify if its a test
+# ADDING     = False                                                                                              # Specify if we adding group and role
+# DELETE     = False                                                                                              # Specify if we deleting group and role
+# 
+# def test():                                                                                                     # Entrypoint function
+#     session = connect_to_database(SERVER, DATABASE)                                                             # Initialize database session
+#     if ADDING:
+#         add_group_to_sql_logins(GROUP_NAME, session)                                                            # Add the group to sql_logins
+#         add_group_to_database_principals(GROUP_NAME, session)                                                   # Add the group to database_principals
+#         add_role_to_group(GROUP_NAME, "db_accessadmin", session)                                                # Attribute the role
+#     if DELETE:
+#         delete_role_from_group(GROUP_NAME, "db_accessadmin", session)                                           # Remove the role from the group
+#         delete_group_from_database_principals(GROUP_NAME, session)                                              # Delete the group from the MSSQL
+# 
+# if IS_TEST:
+#     test()
