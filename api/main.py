ac983ab2b25094abd3c81f1fcf8e2311d51b58fa
David Duchesne <dav.duchesne@cgi.com>
2025-04-14 10:48:33 -0400

    get function

--- api/main.py
+++ api/main.py
@@ -1,13 +1,21 @@
 # https://learn.microsoft.com/en-us/sql/relational-databases/security/authentication-access/database-level-roles?view=sql-server-ver16
 
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import create_engine, select, Column, Integer, String, Table, MetaData, alias
+from fastapi        import FastAPI, Depends, HTTPException
+from sqlalchemy     import create_engine, select, Column, Integer, String, Table, MetaData, alias, literal
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from sqlalchemy.sql import text, quoted_name
+from pydantic       import BaseModel
+from typing         import List
 
-SERVER   = "localhost\\SQLEXPRESS"                                                                                 # MSSQL Server
-DATABASE = "testing"                                                                                               # MSSQL Database
 app      = FastAPI()                                                                                               # FastAPI App
+
+class GroupShema(BaseModel):
+    MemberPrincipalName: str
+    RolePrincipalName: str
+    Server: str
+    Database: str
+    class Config:
+        orm_mode = True
 
 # TABLES
 sql_logins = Table(
@@ -41,6 +49,29 @@ def connect_to_database(server: str, database: str):
     engine = create_engine(f"mssql+pyodbc://@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server", echo=True) # Create the database engine
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)                                    # Initialize the Session local
     return SessionLocal                                                                                            # Return the session
+
+def get_group_from_database(group_name: str, server: str, database: str, SessionLocal: Session):
+    db      = SessionLocal()                                                                                       # Initialize session
+    roles   = alias(database_principals, name="roles")                                                             # Initialize the aliase `roles`
+    members = alias(database_principals, name="members")                                                           # Initialize the aliases `members`
+    query = (                                                                                                      # Build the query
+        select(
+            roles.c.principal_id.label("RolePrincipalID"),
+            roles.c.name.label("RolePrincipalName"),
+            database_role_members.c.member_principal_id.label("MemberPrincipalID"),
+            members.c.name.label("MemberPrincipalName"),
+            literal(server).label("Server"),
+            literal(database).label("Database")
+        )
+        .join(database_role_members, database_role_members.c.role_principal_id == roles.c.principal_id)
+        .join(members, database_role_members.c.member_principal_id == members.c.principal_id)
+        .where(members.c.name == group_name)
+    )
+    result = db.execute(query)                                                                                     # Execute the query
+    #returned = []
+    #or row in result:
+    #   returned += [{"group_name": row.MemberPrincipalName, "role_name": row.RolePrincipalName }]
+    return result.all()
 
 # Group exists function in sys.sql_logins
 def is_group_exists_in_sql_logins(group_name: str, SessionLocal: Session):
@@ -162,35 +193,50 @@ def delete_group_from_database_principals(group_name: str, SessionLocal: Session
     else:
         return 1
 
-IS_TEST = True                                                                                                     # Specify if its a test
-ADDING  = False                                                                                                    # Specify if we adding group and role
-DELETE  = False                                                                                                    # Specify if we deleting group and role
+SERVER     = "localhost\\SQLEXPRESS"                                                                               # MSSQL Server
+DATABASE   = "testing"                                                                                             # MSSQL Database
+GROUP_NAME = "ADSQLGroup4"
+IS_TEST    = False                                                                                                 # Specify if its a test
+ADDING     = False                                                                                                 # Specify if we adding group and role
+DELETE     = False                                                                                                 # Specify if we deleting group and role
+
+## Routes
+@app.get("/server/{server}/database/{database}/group/{group_name}", response_model=List[GroupShema])
+def get_group(server: str, database:str, group_name: str):
+    session = connect_to_database(server, database)                                                               # Initialize database session
+    return get_group_from_database(group_name, server, database, session)
+
+@app.put("/server/{server}/database/{database}/group/{group_name}")
+def update_group(server: str, database:str, group_name: str):
+    # session = connect_to_database(server, database)                                                               # Initialize database session
+    return("UPDATING")
+
+
+# db_accessadmin
+
+@app.post("/server/{server}/database/{database}/group/{group_name}")
+def create_group(server: str, database:str, group_name: str, role_name: str):
+    session = connect_to_database(server, database)                                                               # Initialize database session
+    add_group_to_sql_logins(group_name, session)                                                                  # Add the group to sql_logins
+    add_group_to_database_principals(group_name, session)                                                         # Add the group to database_principals
+    add_role_to_group(group_name, role_name, session)                                                             # Attribute the role
+
+@app.delete("/server/{server}/database/{database}/group/{group_name}")
+def delete_group(server: str, database:str, group_name: str):
+    session = connect_to_database(server, database)                                                               # Initialize database session
+    delete_group_from_database_principals(group_name, session)                                                    # Delete the group from the MSSQL
+
+
 
 def test():                                                                                                        # Entrypoint function
     session = connect_to_database(SERVER, DATABASE)                                                                # Initialize database session
     if ADDING:
-        add_group_to_sql_logins("ADSQLGroup4", session)                                                            # Add the group to sql_logins
-        add_group_to_database_principals("ADSQLGroup4", session)                                                   # Add the group to database_principals
-        add_role_to_group("ADSQLGroup4", "db_accessadmin", session)                                                # Attribute the role
+        add_group_to_sql_logins(GROUP_NAME, session)                                                            # Add the group to sql_logins
+        add_group_to_database_principals(GROUP_NAME, session)                                                   # Add the group to database_principals
+        add_role_to_group(GROUP_NAME, "db_accessadmin", session)                                                # Attribute the role
     if DELETE:
-        delete_role_from_group("ADSQLGroup4", "db_accessadmin", session)                                           # Remove the role from the group
-        delete_group_from_database_principals("ADSQLGroup4", session)                                              # Delete the group from the MSSQL
+        delete_role_from_group(GROUP_NAME, "db_accessadmin", session)                                           # Remove the role from the group
+        delete_group_from_database_principals(GROUP_NAME, session)                                              # Delete the group from the MSSQL
 
 if IS_TEST:
     test()
-
-## Routes
-#@app.get("/server/{server}/database/{database}/group/{group_name}")
-#def get_group(server: str, database:str, group_name: str):
-#    session = connect_to_database(SERVER, DATABASE)                                                               # Initialize database session
-
-#@app.post("/server/{server}/database/{database}/group/{group_name}")
-#def create_group(server: str, database:str, group_name: str):
-#    session = connect_to_database(server, database)                                                               # Initialize database session
-
-#@app.get("/users/{user_id}")
-#def read_user(user_id: int, db: Session = Depends(get_db)):
-#    user = db.query(User).filter(User.id == user_id).first()
-#    if user is None:
-#        raise HTTPException(status_code=404, detail="User not found")
-#    return user
